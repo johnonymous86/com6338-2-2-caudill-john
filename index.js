44465c9161de11bad5fe4507889eee18db6f8e0a
John Caudill <jcaudill86@gmail.com>
2025-05-22 16:49:05 -0400

    updates

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+function greet() {    
+   let name = prompt("What is your name?")
+
+    alert("Hello, " + name)
+
+    let age = prompt("How old are you?")
+
+    parseInt(age)
+
+    let prompt2 = confirm ("If you have had a birthday this year, click 'OK'. If you have not had a birthday this year, click 'Cancel'.")
+
+    if (prompt2) {
+    alert ("The year of your birth is " + (new Date().getFullYear() - age))
+}
+    else {
+    alert ("The year in which you were born is " + (new Date().getFullYear() - age - 1))
+    }
+   
+
+
+
+
+}
